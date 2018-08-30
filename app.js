17fb7a082ce91cf5bf5c14691b9e3e6cd8e30ab0
Noelia Sabando <lolia_15@hotmail.com>
2018-08-30 17:23:28 -0400

    modifico package.json

--- app.js
+++
@@ -1,8 +0,0 @@
-
-mdLinks = require("./src/index")
-
-/* console.log(mdLinks.mdLinks("C:/Users/Noelia/Documents/proyectos laboratoria/pinterest/readme.md", 1)) */
-mdLinks.mdLinks("test/test.md", {validate:true}).then((links)=>{
-  console.log(links)
-});
-
